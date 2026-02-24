59bd8a6a89bc2de20158dabb395cd6f7730b7726
Marie Santillo <127686912+mariesantillo@users.noreply.github.com>
2026-02-24 16:55:03 +0000

    Update run_randomise_resmem.sh by removing SLURM config
    Removed SLURM job configuration from the script.

--- 2m_resmem_model/run_randomise_resmem.sh
+++ 2m_resmem_model/run_randomise_resmem.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=rand_stranet_entropy
-#SBATCH --output=rand_stranetentropy_%j.out
-#SBATCH --error=rand_stranetentropy_%j.err
-#SBATCH --time=02:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=8G
 
-module load fsl  # Load FSL if your system uses modules
+module load fsl  
 
 randomise -i /foundcog/forrestgump/foundcog-infants-2m/resmem_model/second_level/merged_beta_maps/merged_resmem-prediction_4d.nii.gz \
           -o /foundcog/forrestgump/foundcog-infants-2m/resmem_model/second_level/results/oneSam_Resmem_Resmem_Model \
