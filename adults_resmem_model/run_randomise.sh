34b54acfc7a08fe1a31855e5daa4b6c31bf1a172
Marie Santillo <127686912+mariesantillo@users.noreply.github.com>
2026-02-24 17:35:36 +0000

    Add run_randomise.sh script for randomization process

---
+++ adults_resmem_model/run_randomise.sh
@@ -0,0 +1,4 @@
+
+randomise -i /foundcog/forrestgump/foundcog-adult-2/resmem_model/second_level/merged_beta_maps/merged_resmem_4d_2.nii.gz \
+          -o /foundcog/forrestgump/foundcog-adult-2/resmem_model/second_level/results/oneSam_Resmem_Resmem_STRANET_rsm_VCA_Model \
+          -1 -T
