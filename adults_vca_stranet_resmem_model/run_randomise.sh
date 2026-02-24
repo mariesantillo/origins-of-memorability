6ceafbb61f1f115da3dea364bc518ec7108c4aee
Marie Santillo <127686912+mariesantillo@users.noreply.github.com>
2026-02-24 17:39:29 +0000

    Add randomise script for VCA model analysis

---
+++ adults_vca_stranet_resmem_model/run_randomise.sh
@@ -0,0 +1,10 @@
+randomise -i /foundcog/forrestgump/foundcog-adult-2/resmem_stranet_vca_unscoredvideotest/second_level/merged_beta_maps/merged_resmem_4d_2.nii.gz \
+          -o /foundcog/forrestgump/foundcog-adult-2/resmem_stranet_vca_unscoredvideotest/second_level/results/oneSam_Resmem_Resmem_STRANET_rsm_VCA_Model \
+          -1 -T
+randomise -i /foundcog/forrestgump/foundcog-adult-2/resmem_stranet_vca_unscoredvideotest/second_level/merged_beta_maps/merged_rms_difference_4d_2.nii.gz \
+          -o /foundcog/forrestgump/foundcog-adult-2/resmem_stranet_vca_unscoredvideotest/second_level/results/oneSam_attention_rsm_Resmem_STRANET_rsm_VCA_Model \
+          -1 -T
+randomise -i /foundcog/forrestgump/foundcog-adult-2/resmem_stranet_vca_unscoredvideotest/second_level/merged_beta_maps/merged_vca_entropy_4d_2.nii.gz \
+          -o /foundcog/forrestgump/foundcog-adult-2/resmem_stranet_vca_unscoredvideotest/second_level/results/oneSam_vca_entropy_Resmem_STRANET_rsm_VCA_Model \
+          -1 -T
+
